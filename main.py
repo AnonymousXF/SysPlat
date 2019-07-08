b336c6fa4cb49fcc440f1dcbe59a0e958c9700cf
AnonymousXF <464059291@qq.com>
2019-07-08 20:03:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
+from flask import session, g
 from app import create_app
+from app.models import User
 
 app = create_app('dev')
+
+
+@app.before_request
+def make_info_available():
+    if 'user_id' in session:
+        g.user = User.query.filter_by(id=session['user_id']).first()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
