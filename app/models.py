9c3b7cfdc9fa2915e5f4a13787b636a94c663276
AnonymousXF <464059291@qq.com>
2019-07-08 20:05:51 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), nullable=False)
+    username = db.Column(db.String(64), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(64), nullable=True, unique=True)
     role = db.Column(db.Integer, default=0x1)   # 普通用户0x1，admin用户0xf
