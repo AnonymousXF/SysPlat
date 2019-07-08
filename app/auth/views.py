e341f28bb0a02cf1f83f0260742a49d63f7e789d
AnonymousXF <464059291@qq.com>
2019-07-08 20:16:04 +0800

    Update views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, request
-from ..models import db, User
+from flask import render_template, request, url_for, redirect, current_app, session, flash, g
 from . import auth
+from ..models import User
 
 
 @auth.route('/login', methods=['POST', 'GET'])
 def login():
-      if request.method == 'GET':
-          return render_template("login.html")
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        try:
+            user = User.query.filter_by(username=username).first()
+        except Exception as e:
+            current_app.logger.error(e)
+            flash("Inner Error!Something Wrong with Database!")
+            return render_template("login.html")
+        if user is not None and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for("index.dashboard"))
+        else:
+            flash("Wrong Username or Password!")
+            return render_template("login.html")
+
