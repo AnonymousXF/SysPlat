e791a733f494c5b8c36bf0b31c1aca17afe72723
AnonymousXF <464059291@qq.com>
2019-07-08 20:18:00 +0800

    Create views.py

---
+++ app/index/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from flask import render_template, session, g
+from . import index
+
+
+@index.route('/', methods=['GET'])
+def dashboard():
+    return render_template("index.html")
