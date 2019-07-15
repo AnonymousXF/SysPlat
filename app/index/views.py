dd7a2b83e574b51b96b579db05daf6ce3b410cb1
AnonymousXF <464059291@qq.com>
2019-07-15 19:32:26 +0800

    Update views.py

--- app/index/views.py
+++ app/index/views.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, session, g
+from flask import render_template, request, session, g
 from . import index
+from ..decorator import login_required
 
 
 @index.route('/', methods=['GET'])
+@login_required
 def dashboard():
     return render_template("index.html", user=g.user)
+
+
+@index.route('/analysis/nessus', methods=['GET', 'POST'])
+@login_required
+def nessus_analysis():
+    if request.method == 'GET':
+        return render_template('nessus_analysis.html', user=g.user)
