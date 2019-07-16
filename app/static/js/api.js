45b593a2a0780bda813cf54e30a70f30526a2040
AnonymousXF <464059291@qq.com>
2019-07-16 20:16:17 +0800

    Update api.js

--- app/static/js/api.js
+++ app/static/js/api.js
@@ -33,3 +33,30 @@ $('.custom-file-input').on('change', function () {
    let filename = $(this).val().split('\\').pop();
    $(this).next('.custom-file-label').addClass("selected").html(filename);
 });
+
+
+function Analysis(url){
+    // 获取选择的文件
+    let file_obj = $('#scanResultFile').get(0).files[0];
+
+    // 将文件对象打包成form表单类型数据
+    let data = new FormData;
+    data.append('file', file_obj);
+    data.append('level', $('#vul_level').val());
+
+    // 上传文件数据，并进行分析
+    $.ajax({
+        type: 'POST',
+        url: url,
+        data: data,
+        processData:false,
+        contentType:false,
+        dataType: 'html',
+        success: function (data) {
+            $('#nessus_result').html(data);
+        },
+        error: function () {
+            $('#nessus_result').html('分析失败！');
+        }
+    });
+}
