e4a40a8b5e438b74ca683869ee534e6c1e3f4891
AnonymousXF <464059291@qq.com>
2019-07-16 15:36:41 +0800

    Update api.js

--- app/static/js/api.js
+++ app/static/js/api.js
@@ -28,3 +28,8 @@ function PostRegisterInfo(url) {
         }
     });
 }
+
+$('.custom-file-input').on('change', function () {
+   let filename = $(this).val().split('\\').pop();
+   $(this).next('.custom-file-label').addClass("selected").html(filename);
+});
